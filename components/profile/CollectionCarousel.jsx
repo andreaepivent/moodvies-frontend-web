be6695ecd644fb4160298a333aee2e9d023edbd4
Louis Chatenay <louis.chatenay11@gmail.com>
2024-05-28 12:48:20 +0200

    redux movies in history OK

--- components/profile/CollectionCarousel.jsx
+++ components/profile/CollectionCarousel.jsx
@@ -7,9 +7,11 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carouselProfile"; // Importing Carousel components
+import { useSelector } from "react-redux";
 
 // Functional component to display a carousel of movie collections
 function CollectionCarousel() {
+  const movies = useSelector((state) => state.movies.value)
   movies.forEach(movie => {
     console.log(movie)
   })
@@ -26,8 +28,8 @@ function CollectionCarousel() {
             <div className="relative image-container overflow-hidden rounded-lg h-40 w-full group hover:cursor-pointer">
               <img
                 className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
-                src={`/movie/${movie.background}`} // Movie background image
-                alt={`${movie.title} poster`} // Alt text for accessibility
+                src={`/movie/${movie}.jpg`} // Movie background image
+                alt={`${movie} poster`} // Alt text for accessibility
               />
               <div className="absolute bottom-0 left-0 right-0 text-white text-center p-2 opacity-0 transform translate-y-full transition-all duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-0 group-hover:delay-200">
                 {movie.title}
