56655f7c6140d3c5533d665d8e1994b09ff2046d
Andréa <andreaepivent>
2024-05-28 15:37:13 +0200

    remove bug chewbaca

--- components/profile/Informations.jsx
+++ components/profile/Informations.jsx
@@ -52,7 +52,7 @@ return isEditClicked ? (
   <div className="w-screen h-screen bg-radial-gradient flex flex-col justify-around items-center">
     {/* Navbar and header section */}
     <div className="fixed top-7 h-[25%] w-full">
-      <ResponsiveNavbarProfile />
+      <ResponsiveNavbarProfile /> 
       <div className="w-full flex flex-col items-center mt-10 md:flex-row md:justify-between md:items-end">
         <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-300 to-fuchsia-900 text-5xl mb-4 md:mb-0 md:pl-20">
           Hello {user.username}
